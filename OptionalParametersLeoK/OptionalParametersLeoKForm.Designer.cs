ad0c9b799c78ca7014187dc256b84aab3b6ee078
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-04-20 14:27:18 -0400

    Initial commit

---
+++ OptionalParametersLeoK/OptionalParametersLeoKForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace OptionalParametersLeoK
+{
+    partial class frmOptionalParametersLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAptNum = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtAptNum = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblAptNum
+            // 
+            this.lblAptNum.AutoSize = true;
+            this.lblAptNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAptNum.Location = new System.Drawing.Point(33, 22);
+            this.lblAptNum.Name = "lblAptNum";
+            this.lblAptNum.Size = new System.Drawing.Size(93, 16);
+            this.lblAptNum.TabIndex = 0;
+            this.lblAptNum.Text = "Apt Number:";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(56, 52);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(70, 16);
+            this.lblAddress.TabIndex = 1;
+            this.lblAddress.Text = "Address:";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(88, 89);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(38, 16);
+            this.lblCity.TabIndex = 2;
+            this.lblCity.Text = "City:";
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProvince.Location = new System.Drawing.Point(53, 126);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(73, 16);
+            this.lblProvince.TabIndex = 3;
+            this.lblProvince.Text = "Province:";
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPostalCode.Location = new System.Drawing.Point(29, 161);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(97, 16);
+            this.lblPostalCode.TabIndex = 4;
+            this.lblPostalCode.Text = "Postal Code:";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(106, 204);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtAptNum
+            // 
+            this.txtAptNum.Location = new System.Drawing.Point(132, 22);
+            this.txtAptNum.Name = "txtAptNum";
+            this.txtAptNum.Size = new System.Drawing.Size(100, 20);
+            this.txtAptNum.TabIndex = 6;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(132, 52);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(132, 89);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 8;
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Location = new System.Drawing.Point(132, 122);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(100, 20);
+            this.txtProvince.TabIndex = 9;
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(132, 161);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtPostalCode.TabIndex = 10;
+            // 
+            // frmOptionalParametersLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtAptNum);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblAptNum);
+            this.Name = "frmOptionalParametersLeoK";
+            this.Text = "Optional Parameters by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAptNum;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtAptNum;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.TextBox txtPostalCode;
+    }
+}
+
---
+++ OptionalParametersLeoK/OptionalParametersLeoKForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptionalParametersLeoK
+{
+    public partial class frmOptionalParametersLeoK : Form
+    {
+        //variables
+        String userAptNum;
+        String userAddress;
+        String userCity;
+        String userProvince;
+        String userPostalCode;
+
+        public frmOptionalParametersLeoK()
+        {
+            InitializeComponent();
+        }
+
+        public void PrintInformation(String aptNum, String address, String city, String province, String postalCode)
+        {
+            //display information
+            MessageBox.Show("Apt Number: " + aptNum + ", Address: " + address + ", City: " + city + ", Province: " + province + ", Postal Code: " + postalCode, "Information Program");
+        }
+
+        public void PrintInformation(String address, String city, String province, String postalCode)
+        {
+            //display information
+            MessageBox.Show("Address: " + address + ", City: " + city + ", Province: " + province + ", Postal Code: " + postalCode, "Information Program");
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //get the users inputs
+            userAptNum = txtAptNum.Text;
+            userAddress = txtAddress.Text;
+            userCity = txtCity.Text;
+            userProvince = txtProvince.Text;
+            userPostalCode = txtPostalCode.Text;
+
+            //check to see if they haven't entered something
+            if (userAddress == "")
+            {
+                MessageBox.Show("Please enter your address", "Information Program");
+            }
+            else if (userCity == "")
+            {
+                MessageBox.Show("Please enter your city", "Information Program");
+            }
+            else if (userProvince == "")
+            {
+                MessageBox.Show("Please enter your city", "Information Program");
+            }
+            else if (userPostalCode == "")
+            {
+                MessageBox.Show("Please enter your Postal Code", "Information Program");
+            }
+            else if (userAptNum == "")
+            {
+                PrintInformation(userAddress, userCity, userProvince, userPostalCode);
+            }
+            else
+            {
+                PrintInformation(userAptNum, userAddress, userCity, userProvince, userPostalCode);
+            }
+        }
+    }
+}
